6963c60790fe049883a6c1847839c3dd836ebaed
V00771 <valentintino60@gmail.com>
2026-07-27 15:48:05 +0200

    Refactor service worker caching logic and assets

--- sw.js
+++ sw.js
@@ -1,41 +1,26 @@
-const CACHE_NAME = "stundenzettel-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE="stundenzettel-v4-final-handyfix";
+const FILES=["./","./index.html","./style.css","./script.js","./manifest.json"];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener("install",e=>{
   self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request)
-          .then((response) => {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-            return response;
-          })
-          .catch(() => cached)
-      );
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>{
+      return r || fetch(e.request).then(res=>{
+        return caches.open(CACHE).then(c=>{
+          c.put(e.request,res.clone());
+          return res;
+        });
+      });
     })
   );
 });
