356d47015c619597dd7dcc8ea6f8ec7ac370691a
V00771 <valentintino60@gmail.com>
2026-07-27 16:04:28 +0200

    Update service worker cache version to v6-final

--- sw.js
+++ sw.js
@@ -1,14 +1,5 @@
-const CACHE="stundenzettel-v5-fix3";
+const CACHE="stundenzettel-v6-final";
 const FILES=["./","./index.html","./style.css","./script.js","./manifest.json"];
-self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-  );
-});
-self.addEventListener("fetch",e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
