81efeddf9ebfd89b70a53d2d44b8ba5e03cf9ece
V00771 <valentintino60@gmail.com>
2026-07-27 15:52:14 +0200

    Update service worker cache version and fetch strategy

--- sw.js
+++ sw.js
@@ -1,26 +1,14 @@
-const CACHE="stundenzettel-v4-final-handyfix";
+const CACHE="stundenzettel-v5-fix3";
 const FILES=["./","./index.html","./style.css","./script.js","./manifest.json"];
-
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
-
 self.addEventListener("activate",e=>{
   e.waitUntil(
     caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
   );
 });
-
 self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>{
-      return r || fetch(e.request).then(res=>{
-        return caches.open(CACHE).then(c=>{
-          c.put(e.request,res.clone());
-          return res;
-        });
-      });
-    })
-  );
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
